74a74a17fb0afc65135b37cee36bb57ea8c8f380
julianStalman <julianstalmanny@gmail.com>
2025-09-25 10:12:15 +0200

    simplify basic clustering works

--- simple_clustering_setup/mpi_program/write_file.sh
+++ simple_clustering_setup/mpi_program/write_file.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo "Node $(hostname) says hello" >> /home/mpiuser/shared/output.txt
+
+if [[ "$(hostname)" == "node1" || "$(hostname)" == "node2" ]]; then
+  echo "Clustered Node $(hostname) says hello at $(date)" >> /home/mpiuser/shared/output.txt
+else
+  echo "Node $(hostname) is observing the file:"
+  cat /home/mpiuser/shared/output.txt
+fi
